aabfa07ac689d7c7aa787df242dbece9e19a33d3
Suraj Karki <suraj.karki500@gmail.com>
2020-11-20 09:51:18 +0545

    Created Header component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { Container } from "reactstrap";
 
 import "./App.scss";
+import { Header } from "./views/index";
 
 function App() {
   return (
     <React.Fragment>
-      <div className="bg-app w-100"></div>
+      <div className="bg-app w-100">
+        <Container className="content p-3 pt-4">
+          <Header />
+        </Container>
+      </div>
     </React.Fragment>
   );
 }
