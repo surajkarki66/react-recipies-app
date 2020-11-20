5817cdddbdf731e96da480ee0e701c3c9bbb5999
Suraj Karki <suraj.karki500@gmail.com>
2020-11-20 10:59:11 +0545

    Create Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Container } from "reactstrap";
 
 import "./App.scss";
-import { Header } from "./views/index";
+import { Header, Footer } from "./views/index";
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
         <Container className="content p-3 pt-4">
           <Header />
         </Container>
+        <Footer />
       </div>
     </React.Fragment>
   );
---
+++ src/views/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./Footer.scss";
+
+const Footer: React.FC = () => (
+  <footer className="d-flex justify-content-center">
+    <div className="pb-2 pt-3">
+      &copy; {new Date().getFullYear()}, made with{" "}
+      <i className="fa fa-heart heart" /> by{" "}
+      <a href="http://github.com/surajkarki66">Suraj Karki</a>
+    </div>
+  </footer>
+);
+
+export { Footer };
